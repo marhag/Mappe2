f0d7d40c3e02f4223cb4b5659defb373a5bd697f
marhag <martin.hagenjr@hotmail.com>
2014-10-16 15:03:01 +0200

    Initial commit

---
+++ app/src/androidTest/java/android/hioa/s178816_s188098_mappe2/ApplicationTest.java
@@ -0,0 +1,13 @@
+package android.hioa.s178816_s188098_mappe2;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
