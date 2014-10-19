eb621ca8ce78744d3803ef267759736ff0555070
marhag <martin.hagenjr@hotmail.com>
2014-10-19 15:48:14 +0200

    added tasks page, remember to delete

---
+++ app/src/main/java/android/hioa/s178816_s188098_mappe2/TasksLeft.java
@@ -0,0 +1,20 @@
+package android.hioa.s178816_s188098_mappe2;
+
+/**
+ * Created by marhag on 19.10.14.
+ *
+ * Class only for help keeping track with tasks left
+ *
+ * - should be deleted before delivery
+ */
+public class TasksLeft {
+    /*
+    - STYLING!
+    - contentprovider
+        -accessible for other apps
+        -maybe import contacts from phone?
+    - Send sms-function
+    - Settings page
+    - fix onBackPressed ( just quits app now)
+     */
+}
